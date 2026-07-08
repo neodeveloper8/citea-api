2599ed75ba94c0866a19cf6766b3f7966b37d71a
Gonzalo Sierra <gonzaloast8@gmail.com>
2026-07-08 17:39:56 -0500

    feat: endpoint publico GET /api/categories/

--- businesses/views.py
+++ businesses/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return Category.objects.filter(is_active=True).order_by("order")
